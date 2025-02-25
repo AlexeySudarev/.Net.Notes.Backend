e46f57254412d84b0f6aa81ccd939e027712f2a2
Aleksei Sudarev <aleksei.sudarev96@gmail.com>
2025-02-25 08:35:27 +0300

    Add custom exception for not found entity

---
+++ Notes.Application/Utils/Exceptions/NotFoundException.cs
@@ -0,0 +1,7 @@
+﻿namespace Notes.Application.Utils.Exceptions
+{
+    class NotFoundException : Exception
+    {
+        public NotFoundException(string name, object key) : base($"Entity \"{name}\" ({key}) not found.") { }
+    }
+}
