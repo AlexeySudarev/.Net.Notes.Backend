90c9a886a84f0c0a7e072f9ce1b327af45626fa9
Aleksei Sudarev <aleksei.sudarev96@gmail.com>
2025-02-25 08:36:09 +0300

    Add creating, deleting, updating commands for notes

---
+++ Notes.Application/Notes/Commands/DeleteNote/DeleteNoteCommand.cs
@@ -0,0 +1,10 @@
+﻿using MediatR;
+
+namespace Notes.Application.Notes.Commands.DeleteNote
+{
+    class DeleteNoteCommand: IRequest
+    {
+        public Guid Id { get; set; }
+        public Guid UserId { get; set; }
+    }
+}
